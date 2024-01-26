f20cd66a854fc53653fea349838f997a369ee482
OK_Kehan <tang.keh@northeastern.edu>
2024-01-26 10:49:58 -0800

    first try

--- work/03-basic-js/compare.js
+++ work/03-basic-js/compare.js
@@ -5,6 +5,29 @@ module.exports = compare; // DO NOT MODIFY - USED FOR TESTING
 function compare( word, guess ) {  // DO NOT MODIFY
 
 /* YOU MAY MODIFY THE LINES BELOW */
+// num is the numbers of common letters in two words
+let num = 0;
 
-  return 0; // this line is wrong
+// convert each char in word to lowercase and store it in a list
+const wordList = [];
+for (const c of word.toLowerCase()){
+  wordList.push(c);
+}
+
+for (const s of guess.toLowerCase()){
+  // check if the guess number in the wordlist
+  if(wordList.includes(s)){
+    num += 1;
+
+    // get the index of this character in wordlist 
+    // and remove its occurrence from wordList
+    const index = wordList.indexOf(s);
+    wordList.splice(index, 1);
+  }
+  // if not we continue to check next character
+  else{
+    continue;
+  }
+}
+  return num; 
 }
